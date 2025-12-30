25df9b28e29c358ec265eda43377726c93c86fee
Lit_to <litto0315@gmail.com>
2025-12-30 20:24:14 +0900

    add:URLを追加するための関数を追加

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "atlink" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('atlink.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from AtLink!');
-	});
-
-	context.subscriptions.push(disposable);
+import * as vscode from "vscode";
+/**
+ * 拡張機能が有効になったときに起動する関数
+ * 各種機能をcontextに追加する
+ * @param context 自動で挿入されるコンテキストオブジェクト
+ * @returns void
+ */
+function activate(context: vscode.ExtensionContext) {
+  const events = generateEvents();
+  for (let i = 0; i < events.length; ++i) {
+    context.subscriptions.push(events[i]);
+  }
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+/**
+ * この拡張機能で有効になるイベントを生成する
+ * @returns 配列
+ */
+function generateEvents() {
+  let events = Array(0);
+  events.push(vscode.workspace.onDidChangeTextDocument(addUrl));
+  return events;
+}
+
+function addUrl() {}
+
+function deactivate() {
+  return;
+}
+
+export { activate, deactivate };
